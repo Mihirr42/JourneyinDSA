9726fe7ba587d12826dff6b5703b9b5b9ae4406b
Mihir Kumar Shrestha <121042506+Mihirr42@users.noreply.github.com>
2023-09-30 12:57:11 +0530

    insertionAtPosition.cpp

--- LinkedList/insertionAtPosition.cpp
+++ LinkedList/insertionAtPosition.cpp
@@ -1,52 +1,59 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
     Node *next;
 
-    Node(int data){
-        this -> data = data;
-        this -> next = NULL;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
     }
 };
 
-void insertAtHead(Node* &head , int d){
+void insertAtHead(Node *&head, int d)
+{
     Node *temp = new Node(d);
-    temp -> next = head; 
+    temp->next = head;
     head = temp;
 }
 
-void insertAtTail(Node* &tail , int d){
+void insertAtTail(Node *&tail, int d)
+{
     Node *temp = new Node(d);
-    tail -> next = temp;
+    tail->next = temp;
     tail = temp;
 }
 
-// INSERTION AT POSITION ALGORITHM
-void insertAtPosition(Node* &head , Node* &tail , int position , int d){
-    if(position == 1){
-        insertAtHead(head , d);
+void insertAtPosition(Node *&head, Node *&tail, int position, int d)
+{
+    if (position == 1)
+    {
+        insertAtHead(head, d);
         return;
     }
 
-    Node * temp = head;
+    Node *temp = head;
     int count = 1;
 
-    while(count < position - 1){
-        temp = temp -> next;
+    while (count < position - 1)
+    {
+        temp = temp->next;
         count++;
     }
 
-    if(temp -> next == NULL){
-        insertAtTail(tail , d);
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, d);
         return;
     }
 
     Node *nodeToInsert = new Node(d);
-    nodeToInsert -> next = temp -> next;
-    temp -> next = nodeToInsert;
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
 }
 
 
