3f1e0a32e40f468499a69ff9373dc09b6295f80a
Mihir Kumar Shrestha <121042506+Mihirr42@users.noreply.github.com>
2023-09-30 08:02:24 +0530

    selectionsort.cpp

--- Sorting and Searching/selectionsort.cpp
+++ Sorting and Searching/selectionsort.cpp
@@ -8,8 +8,8 @@ void selectionSort(int arr[], int size)
     {
 
         int minIndex = i;
-        for (int j = i + 1; j < size; j++)
-        {
+        for (int j = i + 1; j < size; j++)                                    // TIME COMPLEXITY =  O(n^2)
+        {                                                                     // SPACE COMPLEXITY = O(n)
             if (arr[j] < arr[minIndex])
                 minIndex = j;
         }
