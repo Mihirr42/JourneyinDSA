47e7cab4b285100a2509d905a01f1c7435dc531e
Mihir Kumar Shrestha <121042506+Mihirr42@users.noreply.github.com>
2023-09-30 06:58:17 +0530

    binarysrch.cpp

--- Sorting and Searching/binarysrch.cpp
+++ Sorting and Searching/binarysrch.cpp
@@ -1,1 +1,60 @@
+#include <iostream>
+using namespace std;
 
+// BINARY SEARCH ALGORITHM
+int binarySearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = start + (end - start) / 2;
+
+    // LOOP FOR COMPARING THE KEY ELEMENT WITHIN THE ARRAY
+    while (start <= end)
+    {
+        if (key == arr[mid])
+            return mid;
+
+        else if (key > arr[mid])
+            start = mid + 1;
+
+        else
+            end = mid - 1;
+
+        mid = start + (end - start) / 2;    // RESETTING THE MID POSITION
+    }
+    return -1;                              // IF KEY NOT FONUD
+}
+
+// USER INPUT 
+int main()
+{
+    int size;
+    cout << "Enter size of the array : ";
+    cin >> size;
+
+    int array[size];
+
+    cout << "Enter array elements : ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> array[i];
+    }
+
+    int key;
+    cout << "Enter the key element to search in array : ";
+    cin >> key;
+
+    int index = binarySearch(array, size, key);
+    cout << "Index of " << key << " is " << index << endl;
+
+    cout << "Array : ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i] << " ";
+    }
+
+    return 0;
+}
+
+
+//MIHIRR42
