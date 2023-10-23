a8969b7a47459e48933b1e31c0ec09de6421172a
Mihir Kumar Shrestha <121042506+Mihirr42@users.noreply.github.com>
2023-10-23 08:05:23 +0530

    Heapify Algorithm

---
+++ Heaps/Heapify.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void Heapify(int arr[], int size, int index){
+
+    int largest = index;
+    int left = 2*index;             // left child
+    int right = 2*index+1;           // right child
+
+// CHECKING FOR CORRECT POSITION
+    if(left<=size && arr[largest] < arr[left])
+        largest = left;
+
+    if(right<=size && arr[largest] < arr[right])
+        largest = right;
+
+// IF FOUND THEN SWAP IT WITH ROOT ELEMENT
+    if(largest != index){
+        swap(arr[largest] , arr[index]);
+        // AGAIN CALL THE FUNCTION TO VERIFY IT AFTER SWAPPING
+        Heapify(arr , size, largest);
+    }
+}
+
+
+int main(){
+    int arr[6] = {-1, 54, 53, 55, 52, 50};
+    int n=5;
+
+    // Heap Creation
+    for(int i=n/2; i>0; i--)
+        Heapify(arr, n, i);
+
+    cout<<"Printing the array : ";
+    for(int i=1; i<=n; i++)
+        cout<<arr[i]<<" ";
+
+    return 0;
+}
+
+
+// MIHIRR42
